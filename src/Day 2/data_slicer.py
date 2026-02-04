c24b468660d02cc5b4ce3b2ebecf99d760d39a6e
havyasb69-coder <havyasb69@gmail.com>
2026-02-04 10:51:58 +0530

    Third Commit

---
+++ src/Day 2/data_slicer.py
@@ -0,0 +1,5 @@
+temparature = [22,24,25,28,30,29,27,26,24,22]
+print("First Reading:", temparature[0])
+print("Last Reading:", temparature[-1])
+print("Afternoon Peak Temperature:", temparature[3:6])
+print("Last 3 hours Temperature:", temparature[-3:])
