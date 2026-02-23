9998e85bd38cb79f2a7f71e8cc2568cb11a98fc3
havyasb69-coder <havyasb69@gmail.com>
2026-02-23 14:44:03 +0530

    Learning Data Science with AI/ML

---
+++ src/Day 19/plots.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+
+def simple_plot():
+    x = [1, 2, 3, 4, 5]
+    y = [2, 4, 6, 8, 10]
+
+    plt.plot(x, y, marker='o')
+    plt.title("Feature Branch Visualization")
+    plt.xlabel("X values")
+    plt.ylabel("Y values")
+    plt.grid(True)
+    plt.show()
+
+if __name__ == "__main__":
+    simple_plot()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
